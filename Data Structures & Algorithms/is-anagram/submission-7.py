e4da56beba303810d8af9c1d245ed47386420e33
Michal Gromek <156183984+mjgromek@users.noreply.github.com>
2026-08-03 14:33:48 +0200

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash1, hash2 = defaultdict(int), defaultdict(int)
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            hash1[s[i]] += 1
+            hash2[t[i]] += 1
+        
+        return False if hash1 != hash2 else True
